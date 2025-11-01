48f8aea8dfbef5f6fb6df7034b9e8ba1bec706b3
sustaingineering-elec <ubc.sustaingineering.electrical@gmail.com>
2025-11-01 13:28:33 -0700

    Added the AM2310B Sensor Code

--- MicrocontrollerTesting/main.py
+++ MicrocontrollerTesting/main.py
@@ -4,6 +4,8 @@ import time
 from adafruit_dps310.basic import DPS310 #DPS310
 import adafruit_sht4x #SHT45
 import adafruit_ina228 #INA228
+import adafruit_ahtx0 #AHTX0
+import adafruit_dht #am2301b 
 
 #Board setup
 i2c = board.I2C()
@@ -19,6 +21,10 @@ sht.mode = adafruit_sht4x.Mode.NOHEAT_HIGHPRECISION
 
 #INA228
 ina228 = adafruit_ina228.INA228(i2c)
+
+#AHTX0
+#ahtx0 = adafruit_ahtx0.AHTx0(i2c)
+dht_device = adafruit_dht.DHT22(board.D4)
 
 while True: #FOR CSV WRITING
 
@@ -38,57 +44,63 @@ while True: #FOR CSV WRITING
     ina_energy = ina228.energy
     ina_die_temp = ina228.die_temperature
 
+    #AM2301B
+    am_c = dht_device.temperature
+    am_hum = dht_device.humidity
+
     #Timestamp
     timestamp = time.monotonic() # Using monotonic() is often better for duration on MCUs
 
-    print(f"{timestamp:.3f},{sht_temp:.2f},{sht_rh:.2f},{dps_temp:.2f},{dps_press:.2f},{ina_current:.2f},{ina_bus:.2f},{ina_shunt:.2f},{ina_power:.2f},{ina_energy:.2f},{ina_die_temp:.2f}\n")
+    print(f"{timestamp:.3f},{sht_temp:.2f},{sht_rh:.2f},{dps_temp:.2f},{dps_press:.2f},{ina_current:.2f},{ina_bus:.2f},{ina_shunt:.2f},{ina_power:.2f},{ina_energy:.2f},{ina_die_temp:.2f},{am_c:.2f},{am_hum:.2f}\n")
     
+    #ahtx0_temp = ahtx0.temperature
+    #print("\nAHTX0 Temperature: %0.1f C" % ahtx0.temperature)
     time.sleep(1.0)
 
-""" HUMAN-READABLE OUTPUT - NOT MEANT FOR CSV
-while True:
-    temperature, relative_humidity = sht.measurements
-    print("\nSHT45 Measurements:")
-    print(f"Temperature: {temperature:.1f}C")
-    print(f"Humidity: {relative_humidity:.1f}%")
-    print("----------------------------------------")
+# """ HUMAN-READABLE OUTPUT - NOT MEANT FOR CSV
+# while True:
+#     temperature, relative_humidity = sht.measurements
+#     print("\nSHT45 Measurements:")
+#     print(f"Temperature: {temperature:.1f}C")
+#     print(f"Humidity: {relative_humidity:.1f}%")
+#     print("----------------------------------------")
 
-    print("\nDPS310 Measurements:")
-    print("Temperature = %.2f *C" % dps310.temperature)
-    print("Pressure = %.2f hPa" % dps310.pressure)
-    print("----------------------------------------")
+#     print("\nDPS310 Measurements:")
+#     print("Temperature = %.2f *C" % dps310.temperature)
+#     print("Pressure = %.2f hPa" % dps310.pressure)
+#     print("----------------------------------------")
 
-    print("\nINA 228 Measurements:")
-    print(f"Current: {ina228.current:.2f} mA")
-    print(f"Bus Voltage: {ina228.bus_voltage:.2f} V")
-    print(f"Shunt Voltage: {ina228.shunt_voltage*1000:.2f} mV")
-    print(f"Power: {ina228.power:.2f} mW")
-    print(f"Energy: {ina228.energy:.2f} J")
-    print(f"Temperature: {ina228.die_temperature:.2f} °C")
-    print("----------------------------------------")
+#     print("\nINA 228 Measurements:")
+#     print(f"Current: {ina228.current:.2f} mA")
+#     print(f"Bus Voltage: {ina228.bus_voltage:.2f} V")
+#     print(f"Shunt Voltage: {ina228.shunt_voltage*1000:.2f} mV")
+#     print(f"Power: {ina228.power:.2f} mW")
+#     print(f"Energy: {ina228.energy:.2f} J")
+#     print(f"Temperature: {ina228.die_temperature:.2f} °C")
+#     print("----------------------------------------")
 
-    timestamp = time.time()  # seconds since epoch
-    sht_temp, sht_rh = sht.measurements
-    dps_temp = dps310.temperature
-    dps_press = dps310.pressure
-    ina_current = ina228.current
-    ina_bus = ina228.bus_voltage
-    ina_shunt = ina228.shunt_voltage * 1000
-    ina_power = ina228.power
-    ina_energy = ina228.energy
-    ina_die_temp = ina228.die_temperature
+#     timestamp = time.time()  # seconds since epoch
+#     sht_temp, sht_rh = sht.measurements
+#     dps_temp = dps310.temperature
+#     dps_press = dps310.pressure
+#     ina_current = ina228.current
+#     ina_bus = ina228.bus_voltage
+#     ina_shunt = ina228.shunt_voltage * 1000
+#     ina_power = ina228.power
+#     ina_energy = ina228.energy
+#     ina_die_temp = ina228.die_temperature
 
-    print(f"{timestamp:.3f},{sht_temp:.2f},{sht_rh:.2f},{dps_temp:.2f},{dps_press:.2f},{ina_current:.2f},{ina_bus:.2f},{ina_shunt:.2f},{ina_power:.2f},{ina_energy:.2f},{ina_die_temp:.2f}")
-    time.sleep(1.0)
-"""
+#     print(f"{timestamp:.3f},{sht_temp:.2f},{sht_rh:.2f},{dps_temp:.2f},{dps_press:.2f},{ina_current:.2f},{ina_bus:.2f},{ina_shunt:.2f},{ina_power:.2f},{ina_energy:.2f},{ina_die_temp:.2f}")
+#     time.sleep(1.0)
+# """
 
-""" ADAFRUIT RP2040 TESTING
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
-print("Hello World")
-while True:
-    led.value = True
-    time.sleep(0.5)
-    led.value = False
-    time.sleep(0.5)
-"""
+# """ ADAFRUIT RP2040 TESTING
+# led = digitalio.DigitalInOut(board.LED)
+# led.direction = digitalio.Direction.OUTPUT
+# print("Hello World")
+# while True:
+#     led.value = True
+#     time.sleep(0.5)
+#     led.value = False
+#     time.sleep(0.5)
+# """
