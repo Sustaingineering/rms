60467955630ee5f79527ee881b4f21acb584c7e3
Luong Tuan Dat Nguyen <nltdat24@students.cs.ubc.ca>
2026-01-10 11:59:09 -0800

    Data uploading to S3 update

---
+++ data-transferring/upload_file.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+aws s3 cp /home/pi/data/sensor_data.csv/  s3://bucket-rms-data/testing/`
---
+++ data-transferring/upload_to_s3.py
@@ -0,0 +1,17 @@
+import boto3
+from botocore.exceptions import ClientError
+
+BUCKET = "bucket-rms-data"
+LOCAL_FILE = "/home/pi/data/sensor_data.csv" # path inside the raspbery pi
+REMOTE_KEY = "testing/sensor.csv"   # path inside S3 bucket
+
+def upload():
+    s3 = boto3.client("s3")
+    try:
+        s3.upload_file(LOCAL_FILE, BUCKET, REMOTE_KEY)
+        print("File uploaded successfully.")
+    except ClientError as e:
+        print("Upload failed:", e)
+
+if __name__ == "__main__":
+    upload()
